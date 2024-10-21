65cd168ddf7ee0db33245024059afaed147b57e7
Natasha01013 <natasha01013@yandex.ru>
2024-10-21 17:01:36 +0200

    task three with changes

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -6,8 +6,27 @@ jest.mock('../http');
 beforeEach(() => {
     jest.resetAllMocks();
 });
-test('should call getLevel', () => {
-    fetchData.mockReturnValue('https://server/user/${userId}');
-    getLevel(1);
-    expect(fetchData).toBeCalledWith('https://server/user/1')
-})
+
+//тесты для функции getLevel
+test('return user status if ok', () => {
+    fetchData.mockReturnValue({ status: 'ok', level: 5 });
+    const result = getLevel(1);
+    expect(result).toBe('Ваш текущий уровень: 5');
+  });
+
+test('return error if not ok', () => {
+    fetchData.mockReturnValue({ status: 'error' });
+    const result = getLevel(1);
+    expect(result).toBe('Информация об уровне временно недоступна');
+});
+
+test('return error if unknown', () => {
+    fetchData.mockReturnValue({ status: 'unknown' });
+    const result = getLevel(1);
+    expect(result).toBe('Информация об уровне временно недоступна');
+});
+
+//тест для функции fetchData (для 100% покрытия тестами)
+test('must throw an error when calling', () => {
+    expect(() => fetchData('https://server/user/1')).toThrow('Mock this!');
+});
